72e08644210212ba0a42a0e9ebb8a517190ec161
ZacharyTam <zacharytam@ZacharyTams-Mac-mini.local>
2021-02-06 06:04:51 +0800

    ok

--- src/com/cjg/web/DirServlet.java
+++ src/com/cjg/web/DirServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.cjg.web.utils.FileUtils;
+import utils.FileUtils;
 public class DirServlet extends HttpServlet {
     /**
      * 
